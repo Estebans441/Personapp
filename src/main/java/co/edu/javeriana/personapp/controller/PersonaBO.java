9efbbb78c84bd82385129feb78f4f21848e9b4cd
Estebans441 <estebans441@gmail.com>
2022-05-17 19:58:44 -0500

    Arreglo total de vistas y controladores

---
+++ src/main/java/co/edu/javeriana/personapp/controller/PersonaBO.java
@@ -0,0 +1,55 @@
+package co.edu.javeriana.personapp.controller;
+
+import co.edu.javeriana.personapp.model.dao.PersonaDAO;
+import co.edu.javeriana.personapp.model.dao.impl.PersonaDAOImpl;
+import co.edu.javeriana.personapp.model.dto.PersonaDTO;
+import java.util.List;
+
+public class PersonaBO {
+    private PersonaDAO pdao;
+    
+    public Boolean crear(PersonaDTO persona)
+    {
+        this.pdao = new PersonaDAOImpl();
+        PersonaDTO p = this.pdao.create(persona);
+        if(p != null) return true;
+        else return false;
+    }
+    
+    /*public Boolean editar(PersonaDTO persona)
+    {
+        
+    }*/
+    
+    public Boolean eliminar(Long cedula)
+    {
+        this.pdao = new PersonaDAOImpl();
+        Boolean eliminado = this.pdao.delete(cedula);
+        if(eliminado) return true;
+        else return false;
+    }
+    
+    public PersonaDTO findById(Long cedula)
+    {
+        this.pdao = new PersonaDAOImpl();
+        PersonaDTO p = this.pdao.findById(cedula);
+        if(p != null) return p;
+        else return null;
+    }
+    
+    public List<PersonaDTO> findAll()
+    {
+        this.pdao = new PersonaDAOImpl();
+        List<PersonaDTO> p = this.pdao.findAll();
+        if(p != null) return p;
+        else return null;
+    }
+    
+    public Integer contar()
+    {
+        this.pdao = new PersonaDAOImpl();
+        Integer n = this.pdao.count();
+        if(n != null) return n;
+        else return null;
+    }
+}
---
+++ src/main/java/co/edu/javeriana/personapp/controller/TelefonoBO.java
@@ -0,0 +1,5 @@
+package co.edu.javeriana.personapp.controller;
+
+public class TelefonoBO {
+    
+}
